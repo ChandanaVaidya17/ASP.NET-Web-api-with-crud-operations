d70e61fbbf36c0a6d24e69f8542ccf116b9898c6
ChandanaVaidya17 <chandanavaidya172002@gmail.com>
2023-08-30 17:21:10 +0530

    crud commit

---
+++ CrudOperationsInNetCore/CrudOperationsInNetCore/Controllers/BrandController.cs
@@ -0,0 +1,111 @@
+﻿using CrudOperationsInNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandContext _dbContext;
+        public BrandController(BrandContext dbContext)
+        {
+            _dbContext= dbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if(_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Brands.ToListAsync();
+
+        }
+        /// <summary>
+        /// /getperticular id specified values
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            if (_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            var brand = await _dbContext.Brands.FindAsync(id);
+            if (brand == null) 
+            {
+                return NotFound();
+            
+            }
+            return brand;
+
+        }
+        /// <summary>
+        /// /to insert the values to the sql seerver at localhost
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _dbContext.Brands.Add(brand);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrand), new { id = brand.ID }, brand);
+        }
+        //////update operation
+        [HttpPut]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if(id!= brand.ID)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(brand).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();    
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!BrandAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            
+            }
+            return Ok();
+        }
+        private bool BrandAvailable(int id)
+        {
+            return (_dbContext.Brands?.Any(x => x.ID == id)).GetValueOrDefault(); 
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            if (_dbContext.Brands==null)
+            {
+                return NotFound();
+            }
+            var brand = _dbContext.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Brands.Remove(await brand);
+
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
